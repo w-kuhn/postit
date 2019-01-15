31357b3eef96b29b2d2180125260b3842a9aa2f7
w-kuhn <wendybrookskuhn@gmail.com>
2019-01-15 14:50:57 -0600

    add models, form controllers, views

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
-	has_many :posts
-	has_many :comments
+	has_many :posts, dependent: :destroy
+	has_many :comments, dependent: :destroy
+
+	validates :username, presence: true, uniqueness: true
+	validates :password, presence: true, length: {minimum: 6}, on: :create
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190113221151) do
+ActiveRecord::Schema.define(version: 20190114015608) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "comments", force: :cascade do |t|
     t.text     "body"
     t.integer  "user_id"
+    t.integer  "post_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "post_categories", force: :cascade do |t|
+    t.integer  "category_id"
     t.integer  "post_id"
     t.datetime "created_at"
     t.datetime "updated_at"

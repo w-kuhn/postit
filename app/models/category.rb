4bded6738c359c4690120cbc2ef8af759d65bad5
w-kuhn <wendybrookskuhn@gmail.com>
2019-01-15 21:17:09 -0600

    errors, and categories and comments forms

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,5 +2,5 @@ class Category < ActiveRecord::Base
 	has_many :post_categories
 	has_many :posts, through: :post_categories
 
-	validates :name, presence: true, length: {minimum: 5}
+	validates :name, presence: true, length: {minimum: 4}
 end

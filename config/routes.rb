233cb8102c54056a6eeaf5160c1d1ad4b13b2806
w-kuhn <wendybrookskuhn@gmail.com>
2019-01-19 12:56:42 -0600

    add voting on posts and comments

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,16 @@ PostitTemplate::Application.routes.draw do
   post '/login', to: 'sessions#create'
   get '/logout', to: 'sessions#destroy'
 
-  resources :posts, except: [:destroy] do 
-  	resources :comments, only: [:create]
+  resources :posts, except: [:destroy] do
+  	member do 
+  		post 'vote'
+  	end
+
+  	resources :comments, only: [:create] do 
+  		member do 
+  			post 'vote'
+  		end
+  	end
   end
   resources :categories, only: [:new, :create, :show]
 
